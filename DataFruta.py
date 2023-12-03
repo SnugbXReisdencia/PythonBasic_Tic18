1be876f12f26eea49c3fb350fb107e524037938b
SnugbXReisdencia <santoseverlan45@gmail.com>
2023-12-03 12:12:00 -0300

    fragmentando em arquivos o codigo

---
+++ DataFruta.py
@@ -0,0 +1,23 @@
+from .ListaNomes import ListaNomes
+from .ListaDATAs import ListaDatas
+from .ListaSalarios import ListaSalarios
+from .ListaIdades import ListaIdades
+def main():
+    nomes = ListaNomes()
+    datas = ListaDatas()
+    salarios = ListaSalarios()
+    idades = ListaIdades()
+
+    listaListas = [nomes, datas, salarios, idades]
+
+    for lista in listaListas:
+        lista.entradaDeDados()
+        lista.mostraMediana()
+        lista.mostraMenor()
+        lista.mostraMaior()
+        print("___________________")
+
+    print("Fim do teste!!!")
+
+if __name__ == "__main__":
+    main()
---
+++ ListaIdades.py
@@ -0,0 +1,37 @@
+from .AnaliseDadosABS import AnaliseDados
+
+class ListaIdades(AnaliseDados):
+    
+    def __init__(self):
+        super().__init__(type(int))
+        self.__lista = []        
+    
+    def entradaDeDados(self):
+        '''
+        Este método pergunta ao usuários quantos
+        elementos vão existir na lista e depois
+        solicita a digitação de cada um deles
+        '''
+        pass
+    
+    def mostraMediana(self):
+        '''
+        Este método ordena a lista e mostra o
+        elemento que está na metade da lista
+        '''
+        pass    
+    
+    def mostraMenor(self):
+        '''
+        Este método retorna o menos elemento da lista
+        '''
+        pass
+    
+    def mostraMaior(self):
+        '''
+        Este método retorna o maior elemento da lista
+        '''
+        pass
+
+    def __str__(self):
+        pass
