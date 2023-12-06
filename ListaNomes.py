e87ca2f36a2daae988587064a749b61e2917d7d0
danilo775 <danilo.uba@hotmail.com>
2023-12-05 22:13:32 -0300

    atualizando Lista Nomes

--- ListaNomes.py
+++ ListaNomes.py
@@ -3,28 +3,45 @@ from AnaliseDadosABS import AnaliseDados
 class ListaNomes(AnaliseDados):
     def __init__(self):
         super().__init__(type("String"))
-        self.__lista = []        
+        self.__lista = []  
+    @property
+    def lista(self):
+        return self.__lista
+    @lista.setter
+    def lista(self, lista):
+        self.__lista = lista              
     def entradaDeDados(self):
-        n = int(input("Quantidade de nomes: "))
+        try:
+            n = int(input("Quantidade de nomes que deseja adicionar à lista?: "))         
+        except:
+            print("Valor Passado Invalido. Tente novamente.")
+            return
         for _ in range(n):
             nome = input("Digite um nome: ")
             self.__lista.append(nome)
 
     def mostraMediana(self):
-        sorted_lista = sorted(self.__lista)
-        meio = len(sorted_lista) // 2
-        if len(sorted_lista) % 2 == 0:  
-            print("Mediana:", sorted_lista[meio - 1])  
-        else:
+        try:
+            sorted_lista = sorted(self.__lista)
+            meio = len(sorted_lista) // 2
             print("Mediana:", sorted_lista[meio])
-
+        except:
+            print('Insira um Nome a Lista Primeiro!')   
     def mostraMenor(self):
-        print("Menor elemento:", min(self.__lista))
-
+        try:
+            print("Menor elemento:", min(self.__lista))
+        except:
+            print('Insira um Nome a Lista Primeiro!')
     def mostraMaior(self):
-        print("Maior elemento:", max(self.__lista))
+        try:
+            print("Maior elemento:", max(self.__lista))
+        except:
+            print('Insira um Nome a Lista Primeiro!')
     def listarEmOrdem(self):
-        print("Lista em ordem alfabética:", sorted(self.__lista)) 
+        try:
+            print("Lista em ordem alfabética:", sorted(self.__lista)) 
+        except:
+            print('Insira um Nome a Lista Primeiro!')    
     def __str__(self):
         print(", ".join(self.__lista))
     
